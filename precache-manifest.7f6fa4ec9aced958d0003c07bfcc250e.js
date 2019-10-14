b5aa3b475171348c8d2ab0ee048cf65b59451e7c
Robin Lallier <robin.lallier29@gmail.com>
2019-10-14 21:35:05 +0300

    Updates

---
+++ precache-manifest.7f6fa4ec9aced958d0003c07bfcc250e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45e55c3fa655b6153edfb7095e82843a",
+    "url": "/dotk/index.html"
+  },
+  {
+    "revision": "54e98165cf1750cf5759",
+    "url": "/dotk/static/css/2.c7bb94bc.chunk.css"
+  },
+  {
+    "revision": "da9b31de8c5cdde8e4e0",
+    "url": "/dotk/static/css/main.95901566.chunk.css"
+  },
+  {
+    "revision": "54e98165cf1750cf5759",
+    "url": "/dotk/static/js/2.9c036239.chunk.js"
+  },
+  {
+    "revision": "da9b31de8c5cdde8e4e0",
+    "url": "/dotk/static/js/main.a703329f.chunk.js"
+  },
+  {
+    "revision": "5c36530b19e8187ef184",
+    "url": "/dotk/static/js/runtime~main.f71c18ce.js"
+  },
+  {
+    "revision": "e807a6688f210aee203030a1d53a499e",
+    "url": "/dotk/static/media/logo.e807a668.png"
+  }
+]);
